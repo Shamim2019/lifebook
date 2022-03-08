71744b2e0e53bdeabe153e5722fc99246d64f0b8
Shamim2019 <shamimmaloba@gmail.com>
2022-03-08 12:04:25 +0300

    urls.py

---
+++ instagram/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
+from django.contib.auth.views import LoginView,LogoutView
+
+
+
+urlpatterns=[
+    path('',views.indexView,name="home"),
+    path('dashbord/',views.dashboardView,name="dashboard"),
+    path('login/',LoginView.as_view(),name="login_url"),
+    path('register/',views.registerView,name="register_url"),
+    path('logout/',LogoutView.as_view(next_page='dashboard'),name="logout"),
+    path('like',views.like_api,name="like"),
+    path('unlike',views.unlike_api,name="unlike"),
+    path('home',views.homeView,name="home"),
+    path('editprofile',views.editView,name="edit"),
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
