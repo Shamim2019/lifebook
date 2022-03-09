10669870b0da616f7da15abba3c59dff3d2258b3
Shamim2019 <shamimmaloba@gmail.com>
2022-03-09 13:17:34 +0300

    added imports

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+from django.db import models,reverse
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Image(models.Model):
+    photo = models.ImageField(null = False,upload_to="myimage")
+    date = models.DateTimeField(auto_now_add=True)  
+    title= models.CharField(max_length=300, null=True,blank=True)
+    body=models.CharField(max_length=1000,null=True,blank=True)
+    likes = models.ManyToManyField(User,blank=True,related_name='post_likes')
+    unlikes = models.ManyToManyField(User,blank=True,related_name='post_unlikes')
+
+    def get_like_url(self):    
+        return reverse("instagram:likes")
+
+    def get_unlike_url(self):
+        return reverse("instagram:unlike")
+
+class editprofile(models.Model):
+    name=models.CharField(max_length=300, null=True,blank=True)
--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from django. shortcutsimport render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django. contrib.auth.decorators import User,auth
+from django.contib.auth.decorators import login_required
+from django.views.generic import ListView,DetailView
+from.models import Image,editprofile
+from . forms import ImageForm, EditForm
 
-# Create your views here.
+
+
+def indexView(request):
+    return render(request,"index.html")
+
+
+def registerView(request):
+    if request.method =="POST"
+       password1=request.POST('password1')
+       username=request.POST('username')
+
+       user=User.objects.create_user(username=username,password=password1)
+       user.save()
+    else:
+        return render(request,'registration/register.html')
+
+def dashboardView(request):
+    if request.method == "POST"
+       form = ImageForm(request.POST,request.FILES)
+        if form.is_valid():
+           form.save()
+    form = ImageForm()
+    img = Image.objects.all()
+    return render(request,'dashboard.html',{'img':img,'form':form})
+
+def like_api(request):
+    print(request.POST["image_id"])
+    image=Image.objects.get(id=request.POST["image_id"])
+    image.likes.add(equest.user)
+    return redirect('dashboard')
+
+def unlike_api(request):
+    print(request.POST["image_id"])
+    image=Image.objects.get(id=request.POST["image_id"])
+    user=request.user
+    image.unlikes.add(user)
+    return redirect('dashboard')
+
+def homeView(request):
+    img=image.objects.all()  
+    obj = editprofile.objects.all()
+    return render(request,'profile.html',{'img':img,'obj':obj}) 
+
+def editView(request):
+    if request.method =="POST":
+        form=EditForm(request.POST,request.FILES)
+        if form.is_valid():
+    form = EditForm()
+    return render(request,'editprofile.html,'{'form':form})        
